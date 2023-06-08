8762cf48d4f3632b3253c7315add3cf8767ce7f4
Ling Fung Chan <justinlfchan@gmail.com>
2023-06-08 21:20:50 +0100

    setup get all goals route

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// * Connect backend
+// Connect backend
 console.log("Connected backend server.js");
 
 const express = require("express");
@@ -7,5 +7,7 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}\n`));
