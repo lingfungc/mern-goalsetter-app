fa3d77ce84cc1f85930b4e83f74e159eb4fed089
Ling Fung Chan <justinlfchan@gmail.com>
2023-06-08 21:26:15 +0100

    created post put delete routes for goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,4 +5,16 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "Get all goals" });
 });
 
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Create a new goal" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update goal ID: ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete goal ID: ${req.params.id}` });
+});
+
 module.exports = router;
