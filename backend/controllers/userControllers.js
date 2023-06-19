2e8ce28560c20f170e347ab0a6ebe9654a001f57
Ling Fung Chan <justinlfchan@gmail.com>
2023-06-19 13:51:53 +0100

    updated register-user function in user-controller

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -7,9 +7,43 @@ const User = require("../models/userModel");
 // Desc:      Register a user
 // Route:     POST /api/users
 // Access:    Public
-const registerUser = (req, res) => {
-  res.json({ message: "Register a user" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // * Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // * Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    // res.status(201).json(user);
+
+    // * The "_" is a convention to indicate that the "id" is for internal or private user
+    // * Also, the "id" is not meant to be accessed or modified directly by the external code
+    res.status(201).json({ _id: user.id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 // Desc:      Authenticate a user
 // Route:     POST /api/users/login
