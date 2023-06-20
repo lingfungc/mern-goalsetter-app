b1749a214f6105177b08676175488471717b501b
Ling Fung Chan <justinlfchan@gmail.com>
2023-06-20 16:17:58 +0100

    installed react-router-dom and imported in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+// import React from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return <div>App</div>;
