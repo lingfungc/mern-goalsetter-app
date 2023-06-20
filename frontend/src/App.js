759487dfe0d60688eca7cfc49c84de7b34ed59c7
Ling Fung Chan <justinlfchan@gmail.com>
2023-06-20 16:23:26 +0100

    added router routes and route for dashboard login and register in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 // import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 };
 
 export default App;
