513511ff1df06386333af7c851526a1ecbb3715d
Ling Fung Chan <justinlfchan@gmail.com>
2023-06-20 17:48:16 +0100

    npm installed react-icons and applied fa-icons in header.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Router>
         <div className="container">
+          <Header />
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
